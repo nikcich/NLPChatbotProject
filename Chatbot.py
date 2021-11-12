02d3ba2f8ffb293f9c7ae032891c3bcad0e4b510
Nikolas <nrc170001@utdallas.edu>
2021-11-12 14:18:23 -0600

    Yup

--- Chatbot.py
+++ Chatbot.py
@@ -1,67 +1,140 @@
-import pandas as pd
-from nltk.stem import WordNetLemmatizer
-from nltk import word_tokenize
-from nltk.corpus import stopwords
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import numpy
 import random
-
-stop_words = set(stopwords.words("english"))
-lemmatizer = WordNetLemmatizer()
-
-sentinel = "quit"
-
-user_model = {"name": "Human"}
+import json
+import tensorflow
+import tflearn
+import pickle
+tf = tensorflow.compat.v1
 
 
-def main():
+with open("intents.json") as file:
+    data = json.load(file)
 
-    df = pd.read_csv("knowledgebase.csv")
-    df.drop_duplicates()
+newmod = input("New Model? (Y/N):")
 
-    userInput = "boog"
-    while not userInput == sentinel:
-        userInput = input(">>" + user_model["name"] + ": ")
-        if not userInput == sentinel:
-            tokenized_input = process_input(userInput)
-            print(tokenized_input)
-            matches = find_matching_term(tokenized_input, df)
-            resp_idx = select_response(matches)
-            chat_reply(resp_idx, df)
+if newmod.lower() == 'n':
+    with open("data.p", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+else:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))] # is this lables or classes
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.p", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 
-def process_input(input):
+tf.reset_default_graph()
 
-    tokenized_input = word_tokenize(input)
-    for i in range(len(tokenized_input)):
-        w = tokenized_input[i]
-        w = w.lower()
-        # lemmed = lemmatizer.lemmatize(w)
-        tokenized_input[i] = w
-    filtered = [w for w in tokenized_input if not w.lower() in stop_words]
-    return filtered
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+if newmod.lower() == 'n':
+    model.load("model.tflearn")
+else:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    return numpy.array(bag)
+
+def chat():
+    user = {
+        "name":""
+    }
+    previousTag = ''
+
+    print("Say something to get started!\nOr say \" Example Prompts \" to see examples")
+
+    while True:
+        inp = input(">> Human: ")
+        if inp.lower() == 'quit':
+            break
+            
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        if tag == "tellMore" and previousTag != '':
+            tag = previousTag
+        
+        
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+        
+        if tag == "whoami" and user["name"] != '':
+            responses = ['Your name is ' + user["name"]]
+        
 
 
-def find_matching_term(tokens, df):
-    matching = {}
-    for index, value in df.word.items():
-        if value in tokens:
-            matching[index] = value
-    return matching
+        print(">> Chatbot:", random.choice(responses))
+        if tag == "name":
+            print(">> Chatbot:", "What is your name?")
+            inp = input(">> Human: ")
+            user["name"] = inp
+            print(">> Chatbot: Nice to meet you", inp)
+        
 
 
-def select_response(matches):
-    if len(list(matches.items())) > 0:
-        idx, response = random.choice(list(matches.items()))
-    else:
-        idx = -1
-    return idx
+        previousTag = tag
 
-
-def chat_reply(idx, df):
-    if idx >= 0:
-        print(">>Bot:", df.content[idx])
-    else:
-        print(">>Bot: I don't have a response for that...")
-
-
-if __name__ == "__main__":
-    main()
+chat()
